f889ef228255a576953f3bff65efe162b3b8f543
SARATH PRASAD <152839472+Sarathprasad10@users.noreply.github.com>
2024-07-08 21:56:40 +0530

    Commit message

--- JSON/2.php
+++ JSON/2.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Leap Year Checker</title>
 </head>
-<body>
+<body> 
     <h1>Leap Year Checker</h1>
     <label for="year">Enter a year:</label>
     <input type="number" id="year" name="year">
